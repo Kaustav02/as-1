c267b1af180f4ec6398acd869ac9ed6678be20f7
kaustav <kaustav.mondal2003@gmail.com>
2022-10-09 17:21:36 +0530

    third commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let image_url = [
+    'https://images.unsplash.com/photo-1598755257130-c2aaca1f061c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHw%3D&w=1000&q=80',
+    'https://media.wired.com/photos/593261cab8eb31692072f129/master/pass/85120553.jpg',
+    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hippopotamus-extreme-animals-social-1642555668.jpg',
+    'https://i.natgeofe.com/k/c02b35d2-bfd7-4ed9-aad4-8e25627cd481/komodo-dragon-head-on_4x3.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYSe9GWNEPwKjXKrKXpEBRflFhjYkkilHKuQ&usqp=CAU',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe7FaZ1c0jggZP_BwENoJHT8ah5CleWermxg&usqp=CAU',
+    'https://images.unsplash.com/photo-1566650554919-44ec6bbe2518?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhdXRpZnVsJTIwYW5pbWFsfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    'https://i.pinimg.com/originals/44/b9/94/44b9945d2114980afbcbc72a80221f19.jpg',
+    'https://assets3.thrillist.com/v1/image/2498963/792x456/scale;webp=auto;jpeg_quality=60;progressive.jpg',
+    'https://hips.hearstapps.com/rbk.h-cdn.co/assets/17/35/lions.jpg',
+
+]
+
+function change(){
+    
+}
